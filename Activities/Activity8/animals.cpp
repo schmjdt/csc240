26d61f81b121716fc9ef9e1aa96ccf38732a3e2a
Joe Schmidt <gitschmjdt@gmail.com>
2013-06-21 04:55:27 -0700

    Update animals.cpp

--- Activities/Activity8/animals.cpp
+++ Activities/Activity8/animals.cpp
@@ -3,50 +3,54 @@ using namespace std;
 
 
 class Animal {
+protected: 
+	string type;
 public:
  virtual void speak() = 0;
+ virtual void SetType(string value) { type = value; }
+ virtual string GetType() { return type; }
  //void speak() { cout << "Hello\n"; }
 };
 
 class Dog : public Animal {
 public:
+ Dog() { SetType("dog"); }
  void speak() { cout << "Woof\n"; }
 };
 
 class Lion : public Animal {
 public:
+ Lion() { SetType("lion"); }
  void speak() { cout << "Roar\n"; }
 };
 
 class Snake : public Animal {
 public:
+ Snake() { SetType("snake"); }
  void speak() { cout << "Ssss\n"; }
 };
 
 class Narwhal : public Animal {
 public:
+ Narwhal() { SetType("narwhal"); }
  void speak() { cout << "It's not a f*#$! horn\n"; }
 };
 
 int main()
 {
-	Animal *a1, *a2;
+	const int numAnimals = 4;
+	Animal *a1, *a2, *an[numAnimals];
 	int ii;
 
 	Dog rover;
 	Lion Leo;
 	Snake Plissken;
 	Narwhal Buddy;
-
-        a2 = &rover;
-	a2->speak();
-
-	//a[0] = &rover; a[1] = &Leo; a[2] = &Plissken; a[3] = &Buddy;
-	Animal *an[] = {&rover, &Leo, &Plissken, &Buddy};
-	int numAnimals = 4;
-
+	
+	an[0] = &rover; an[1] = &Leo; an[2] = &Plissken; an[3] = &Buddy;
+	
 	for (ii = 0; ii < numAnimals; ii++) {
+		cout << "a " << an[ii]->GetType() << " goes ";
 		an[ii]->speak();
 	} 
-
 }
